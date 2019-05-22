410e1e97f3d6db03b75cf93899c03c49d6eb6f31
Lucas Lima <38673203+lucaslimaax@users.noreply.github.com>
2019-05-22 09:28:28 -0300

    Scripts para executar comandos de ipconfig

---
+++ Scripts comandos redes/comando_redes.bat
@@ -0,0 +1,21 @@
+@echo off
+
+cls
+cls
+cd c:\windows\system32
+cls
+ipconfig /release
+cls
+ipconfig /renew
+cls
+ipconfig /flushdns
+cls
+ipconfig /registerdns
+cls
+gpupdate /force
+
+
+exit
+
+
+
---
+++ Scripts comandos redes/start.bat
@@ -0,0 +1,5 @@
+@echo off
+
+cd "C:\Windows"
+start /min comando_redes
+exit
